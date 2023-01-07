e6f299d897d54e06b186ed66442944234ffbffe6
Dima <D130188@yandex.ru>
2023-01-07 13:07:57 +0400

    plug-in-inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,26 @@
 const editBtn = document.querySelector('.user__edit');
 const closeModalBtn =document.querySelector('.modal__close');
 const popUp = document.querySelector('.modal');
+const inputName = document.querySelector('#name');
+const inputActivity = document.querySelector('#activity');
+const formProfile = document.querySelector('#form-profile');
+const userName = document.querySelector('.user__name');
+const userActivity = document.querySelector('.user__activity');
 
-editBtn.addEventListener('click', () => {
-  popUp.classList.add('modal_open_true');
-})
+inputName.value = userName.textContent;
+inputActivity.value = userActivity.textContent;
 
-closeModalBtn.addEventListener('click', () => {
-  popUp.classList.remove('modal_open_true');
+const toggleModal = () => {
+  popUp.classList.toggle('modal_open_true');
+}
+
+editBtn.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
+
+formProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  userName.textContent = inputName.value;
+  userActivity.textContent = inputActivity.value;
+  toggleModal();
 })
