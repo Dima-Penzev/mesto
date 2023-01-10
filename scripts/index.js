1a9e5931caad8dff137661557eb7dbb04934d9cf
Dima <D130188@yandex.ru>
2023-01-10 12:21:47 +0400

    change-readme

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const editBtn = document.querySelector('.user__edit');
-const closeModalBtn =document.querySelector('.modal__close');
-const popUp = document.querySelector('.modal');
-const inputName = document.querySelector('#name');
-const inputActivity = document.querySelector('#activity');
-const formProfile = document.querySelector('#form-profile');
 const userName = document.querySelector('.user__name');
 const userActivity = document.querySelector('.user__activity');
+const popUp = document.querySelector('.popup');
+const closePopUpBtn =popUp.querySelector('.popup__close');
+const formProfile = popUp.querySelector('#form-profile');
+const inputName = popUp.querySelector('#name');
+const inputActivity = popUp.querySelector('#activity');
 
 inputName.value = userName.textContent;
 inputActivity.value = userActivity.textContent;
 
-const toggleModal = () => {
-  popUp.classList.toggle('modal_opened');
+const togglePopUp = () => {
+  popUp.classList.toggle('popup_opened');
 }
 
-editBtn.addEventListener('click', toggleModal);
-closeModalBtn.addEventListener('click', toggleModal);
+editBtn.addEventListener('click', togglePopUp);
+closePopUpBtn.addEventListener('click', togglePopUp);
 
 formProfile.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   userName.textContent = inputName.value;
   userActivity.textContent = inputActivity.value;
-  toggleModal();
+  togglePopUp();
 })
