76de6e457f704236fdf87efe3abba86fcabaad41
Dima <D130188@yandex.ru>
2023-01-22 23:28:42 +0400

    added template for cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,33 @@
+import { initialCards } from "./initial-cards.js";
 const editorBtn = document.querySelector('.user__edit');
 const userName = document.querySelector('.user__name');
 const userActivity = document.querySelector('.user__activity');
-const popUp = document.querySelector('.popup');
+const cardsContainer = document.querySelector('.cards-container');
+const cardTemplate = document.querySelector('#card').content;
+const addCardBtn = document.querySelector('.user__add-card');
+const popUp = document.querySelector('.popup_type_profile');
 const closePopUpBtn =popUp.querySelector('.popup__close');
 const formProfile = popUp.querySelector('#form-profile');
 const inputName = popUp.querySelector('#name');
 const inputActivity = popUp.querySelector('#activity');
+
+//Функция для создания начального набора карточек
+const makeInitialCardsSet = (images) => {
+  const cardsSet = [];
+  
+  images.map(image => {
+    const cardMarkup = cardTemplate.querySelector('.card').cloneNode(true);
+    cardMarkup.querySelector('.card__image').src=`${image.link}`;
+    cardMarkup.querySelector('.card__image').alt=`${image.name}`;
+    cardMarkup.querySelector('.card__text').textContent=`${image.name}`;
+
+    cardsSet.push(cardMarkup);
+  })
+
+  return cardsSet;
+}
+
+cardsContainer.append(...makeInitialCardsSet(initialCards))
 
 // Функция открытия окна редактирования
 const openPopUp = () => {
@@ -30,3 +52,10 @@ const handleFormSubmit = (evt) => {
 editorBtn.addEventListener('click', openPopUp);
 closePopUpBtn.addEventListener('click', closePopUp);
 formProfile.addEventListener('submit', handleFormSubmit);
+addCardBtn.addEventListener('click', () => {
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__image').src='https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+  newCard.querySelector('.card__image').alt='Архыз';
+  newCard.querySelector('.card__text').textContent='Архыз';
+  cardsContainer.prepend(newCard);
+})
