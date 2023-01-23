eb1d2bd9e85c58fd5ba8400be01a0a22d73eb511
Dima <D130188@yandex.ru>
2023-01-23 16:54:34 +0400

    added feature like button

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const userActivity = document.querySelector('.user__activity');
 const cardsContainer = document.querySelector('.cards-container');
 const addCardBtn = document.querySelector('.user__add-card');
 const popUpProfile = document.querySelector('.popup_type_profile');
-const closePopUpBtn =popUpProfile.querySelector('.popup__close');
+const closePopUpProfileBtn =popUpProfile.querySelector('.popup__close');
 const formProfile = popUpProfile.querySelector('#form-profile');
 const inputName = popUpProfile.querySelector('#name');
 const inputActivity = popUpProfile.querySelector('#activity');
@@ -14,6 +14,8 @@ const closePopUpCardEditorBtn =popUpCardEditor.querySelector('.popup__close');
 const formCardEditor = popUpCardEditor.querySelector('#form-card-editor');
 const inputCardTitle = popUpCardEditor.querySelector('#card-title');
 const inputCardLink = popUpCardEditor.querySelector('#card-link');
+const popUpImage = document.querySelector('.popup_type_image');
+const closePopUpImageBtn = popUpImage.querySelector('.popup__close');
 const cardTemplate = document.querySelector('#card').content;
 
 //Функция для создания начального набора карточек
@@ -71,10 +73,28 @@ const addCardImage = (evt) => {
   formCardEditor.reset();
 }
 
+//Функция создания картинки с подписью в модальном окне
+const makeImageInPopUP = (evt) => {
+    popUpImage.querySelector('.module__image').src = `${evt.target.getAttribute('src')}`;
+    popUpImage.querySelector('.module__image').alt = `${evt.target.getAttribute('alt')}`;
+    popUpImage.querySelector('.module__caption').textContent = `${evt.target.getAttribute('alt')}`;
+}
+
 editorBtn.addEventListener('click', () => {openPopUp(popUpProfile)});
-closePopUpBtn.addEventListener('click', () => {closePopUp(popUpProfile)});
+closePopUpProfileBtn.addEventListener('click', () => {closePopUp(popUpProfile)});
 addCardBtn.addEventListener('click', () => {openPopUp(popUpCardEditor)})
 closePopUpCardEditorBtn.addEventListener('click', () => {closePopUp(popUpCardEditor)});
 formProfile.addEventListener('submit', handleFormSubmit);
 formCardEditor.addEventListener('submit', addCardImage)
+cardsContainer.addEventListener('click', (evt) => {
 
+  if(evt.target.classList.contains('card__image')) {
+    makeImageInPopUP(evt);
+    openPopUp(popUpImage); 
+  }
+
+  if(evt.target.classList.contains('card__like-btn')) {
+    evt.target.classList.toggle('card__like-btn_active');
+  }
+});
+closePopUpImageBtn.addEventListener('click', () => {closePopUp(popUpImage)});
